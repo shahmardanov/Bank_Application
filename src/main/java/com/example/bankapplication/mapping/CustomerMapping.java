95b82a43e05c98f61afa949ed3b8013703910518
shamil <shahmardanov.shamil@gmailcom>
2024-11-07 12:46:52 +0400

    add methods to CustomerMapping interface

--- src/main/java/com/example/bankapplication/mapping/CustomerMapping.java
+++ src/main/java/com/example/bankapplication/mapping/CustomerMapping.java
@@ -1,4 +1,25 @@
 package com.example.bankapplication.mapping;
 
+import com.example.bankapplication.model.Customer;
+import com.example.bankapplication.request.CustomerRequest;
+import com.example.bankapplication.response.CustomerResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface CustomerMapping {
+
+    Customer requestToEntity(CustomerRequest customerRequest);
+
+    CustomerResponse createToResponse(Customer customer);
+
+    CustomerResponse readToResponse(Customer customer);
+
+    CustomerResponse updateToResponse(Customer customer);
+
+    Customer updateCustomerToUpdateResponse(CustomerRequest customerRequest, @MappingTarget Customer customer);
+
+
 }
--- src/main/java/com/example/bankapplication/service/CustomerService.java
+++ src/main/java/com/example/bankapplication/service/CustomerService.java
@@ -1,4 +1,12 @@
 package com.example.bankapplication.service;
 
+import com.example.bankapplication.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
 }
