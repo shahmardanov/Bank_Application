3f77eb4ac8d6badf707a3bf3c823792b8a138263
shamil <shahmardanov.shamil@gmailcom>
2024-11-07 12:49:35 +0400

    add methods to AccountMapping interface

--- src/main/java/com/example/bankapplication/mapping/AccountMapping.java
+++ src/main/java/com/example/bankapplication/mapping/AccountMapping.java
@@ -1,4 +1,22 @@
 package com.example.bankapplication.mapping;
 
+import com.example.bankapplication.model.Account;
+import com.example.bankapplication.request.AccountRequest;
+import com.example.bankapplication.response.AccountResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface AccountMapping {
+    Account requestToEntity(AccountRequest accountRequest);
+
+    AccountResponse createToResponse(Account Account);
+
+    AccountResponse readToResponse(Account Account);
+
+    AccountResponse updateToResponse(Account Account);
+
+    Account updateAccountToUpdateResponse(AccountRequest AccountRequest, @MappingTarget Account Account);
 }
