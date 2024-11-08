59d2cd7fb15fd5221bc02744d2589eadc89d64c1
shamil <shahmardanov.shamil@gmailcom>
2024-11-08 20:42:09 +0400

    add getall method in controller class

--- src/main/java/com/example/bankapplication/controller/CustomerController.java
+++ src/main/java/com/example/bankapplication/controller/CustomerController.java
@@ -4,7 +4,11 @@ import com.example.bankapplication.request.CustomerRequest;
 import com.example.bankapplication.response.CustomerResponse;
 import com.example.bankapplication.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +23,10 @@ public class CustomerController {
     @PostMapping("/create")
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody CustomerRequest customerRequest) {
         return ResponseEntity.ok(customerService.createCustomer(customerRequest));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<CustomerResponse>> getAllCustomers(){
+        return ResponseEntity.ok(customerService.getAllCustomers());
     }
 }
